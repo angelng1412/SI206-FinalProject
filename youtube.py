748316575d8938028d865458c54971a3c5d254a8
angelng1412 <angelng1412@gmail.com>
2022-04-14 05:16:43 -0400

    Write calculations to csv and create scatter plot

--- youtube.py
+++ youtube.py
@@ -90,9 +90,79 @@ def fetchData(db_name):
 
     conn.commit()
 
+def calculate_average_views(db_name):
+    # connect to database
+    conn, cur = database(db_name)
+
+    # get unique artist names
+    artists = cur.execute("SELECT DISTINCT artist FROM musicvideos").fetchall()
+    artists = [x[0] for x in artists] # list of artist names
+
+    avg_views = {}
+    for artist in artists:
+        views = cur.execute("SELECT views FROM musicvideos WHERE artist = ?", (artist,)).fetchall()
+        views = [x[0] for x in views]
+        avg_views[artist] = sum(views) / len(views)
+
+    return avg_views
+
+def calculate_average_likes(db_name):
+    # connect to database
+    conn, cur = database(db_name)
+
+    # get unique artist names
+    artists = cur.execute("SELECT DISTINCT artist FROM musicvideos").fetchall()
+    artists = [x[0] for x in artists] # list of artist names
+
+    avg_likes = {}
+    for artist in artists:
+        likes = cur.execute("SELECT likes FROM musicvideos WHERE artist = ?", (artist,)).fetchall()
+        likes = [x[0] for x in likes]
+        avg_likes[artist] = sum(likes) / len(likes)
+
+    return avg_likes
+
+def write_avgs(filename):
+    pass
+
+def scatterplot(db_name, file):
+
+    # connect to database
+    conn, cur = database(db_name)
+
+    # get artists, followers on spotify, and average views from top music videos
+    results = cur.execute("""SELECT a.artist, a.followers, m.views FROM 
+                            (SELECT artist, AVG(views) as views FROM musicvideos GROUP BY artist) AS m 
+                            JOIN artistinfo AS a 
+                            ON a.artist = m.artist""").fetchall()
+    # print(results)
+    followers = []
+    avg_views = []
+    dir = os.path.dirname(file)
+    outfile = open(os.path.join(dir, file), "w")
+    writer = csv.writer(outfile, delimiter=",")
+    writer.writerow(('artist', 'followers', 'average views'))
+    for row in results:
+        writer.writerow(row) # write artist name, spotify followers, and average views on music videos to csv
+        followers.append(row[1])
+        avg_views.append(row[2])
+    outfile.close()
+
+    fig, ax = plt.subplots()
+    ax.scatter(followers, avg_views, alpha=0.8, edgecolors='none', s=30)
+    ax.ticklabel_format(useOffset=False, style='plain')
+    plt.title('Hello')
+    ax.set_ylabel('Average number of views')
+    ax.set_xlabel('Number of followers on Spotify')
+    plt.tight_layout()
+    plt.savefig("followersviews.png")
+        
 
 def main():
-    fetchData("songstats.db")
+    # fetchData("songstats.db")
+    # avg_views = calculate_average_views("songstats.db")
+    # avg_likes = calculate_average_likes("songstats.db")
+    scatterplot("songstats.db", "followersvsviews.csv")
 
 if __name__ == '__main__':
     main()
