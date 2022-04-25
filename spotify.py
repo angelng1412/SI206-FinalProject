36ae5794e8de83c49991ac606d15f9958d5bb151
kskyekim <kskyekim@umich.edu>
2022-04-25 15:51:08 -0400

    implemented functions

--- spotify.py
+++ spotify.py
@@ -3,6 +3,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 from bs4 import BeautifulSoup
 import urllib.request as urllib
+import csv
+import sqlite3
+import os
 
 
 client_id = input("what's your client id?: ")
@@ -11,12 +14,11 @@ client_secret = input("what's your client secret?: ")
 client_credentials_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-
 class Spotify:
-    def get_data(self, uniqname_dictionary, username_dictionary):
+    def get_data(self, playlist_dictionary):
         fname = input("Enter .csv filename: ")
         try:
-            fhand = pd.read_csv("./files/" + fname)
+            fhand = pd.read_csv(fname)
 
         except:
             print("Cannot open file.")
@@ -30,73 +32,68 @@ class Spotify:
             playlistid = a[1]
 
             # inserts id to dictionary of spotify usernames
-            username_dictionary[fhand["Number"][i]] = playlistid
+            playlist_dictionary[fhand["Number"][i]] = playlistid
 
 
-    def call_playlist(self, username, playlist_id):
-            # this is what each track holds
-            playlist_features_list = ["popularity_index", "album_release_date", "artist", "album","track_name",  "track_id", "danceability",
-                                      "energy","key","loudness","mode", "speechiness","instrumentalness",
-                                      "liveness","valence","tempo", "duration_ms","time_signature"]
+    def playlist_sql(self, db_name, key, playlist_id):
+        path = os.path.dirname(os.path.abspath(__file__))
+        conn = sqlite3.connect(path+'/'+db_name)
+        cur = conn.cursor()
 
-            # begins to create the data table
-            playlist_df = pd.DataFrame(columns = playlist_features_list)
+        # create the table
+        cur.execute("DROP TABLE IF EXISTS songstats")
+        cur.execute("CREATE TABLE songstats (artist TEXT PIMARY KEY, track_name TEXT, danceability NUMERIC, energy NUMERIC, tempo NUMERIC)")
 
-            # searches the playlist
-            print(username)
-            print(playlist_id + '\n')
+        # searches the playlist
+        print(key)
+        print(playlist_id + '\n')
 
-            # makes it so returns more than limit = 100
-            tracks = sp.user_playlist_tracks(user = username, playlist_id = playlist_id)["tracks"]
-            playlist = tracks["items"]
-            while (tracks["next"]):
-                tracks = sp.next(tracks)
-                playlist.extend(tracks["items"])
+        # gathers the json file data
+        tracks = sp.user_playlist_tracks(user = key, playlist_id = playlist_id)["tracks"]
+        playlist = tracks["items"]
 
-    
-            for track in playlist:
-                # get playlist features
-                playlist_features = {}
-            
-                if (track["track"] == None):
-                    continue
+        # for loop size
+        size = 25
 
-                print(track["track"]["popularity"])
-                playlist_features["popularity_index"] = track["track"]["popularity"]
-                playlist_features["album_release_date"] = track["track"]["album"]["release_date"]
-                playlist_features["artist"] = track["track"]["artists"][0]["name"]
-                playlist_features["album"] = track["track"]["album"]["name"]
-                playlist_features["track_name"] = track["track"]["name"]
-                playlist_features["track_id"] = track["track"]["id"]
+        if (len(playlist) < 25):
+            size = len(playlist)
 
-                # audio features
-                audio_features = sp.audio_features(playlist_features["track_id"])[0]
+        for track in playlist[:size]:
+            # if can't find track... continue
+            if (track["track"] == None):
+                 continue
 
-                for feature in playlist_features_list[6:]:
-                    playlist_features[feature] = audio_features[feature]
+            # gets audio features
+            audio_features = sp.audio_features(track["track"]["id"])[0]
 
+            danceability = audio_features["danceability"]
+            energy = audio_features["energy"]
+            tempo = audio_features["tempo"]
 
-                track_df = pd.DataFrame(playlist_features, index = [0])
-                playlist_df = pd.concat([playlist_df, track_df], ignore_index = True)
+            cur.execute(
+                """
+                INSERT OR IGNORE INTO songstats (artist, track_name, danceability, energy, tempo)
+                VALUES (?, ?, ?, ?, ?)
+                """,
+                (track["track"]["artists"][0]["name"], track["track"]["name"], danceability, energy, tempo)
+            )
 
-            return playlist_df
+        conn.commit()
 
 
 def main():
     a = Spotify()
-    uniqname_dictionary = dict()
-    username_dictionary = dict()
+    playlist_dictionary = dict()
 
-    a.get_data(uniqname_dictionary, username_dictionary)
+    a.get_data(playlist_dictionary)
 
     # function for reading through all individual playlists
-    for i in range(len(username_dictionary)):
+    for i in range(len(playlist_dictionary)):
         # get key and playlist id from list... key is just Spotify's playlists
         key = "Spotify"
-        id = list(username_dictionary.values())[i]
+        id = list(playlist_dictionary.values())[i]
 
-        # exports tracks to .csv file w/ uniqname as filename
-        folder_to_export_path = "./output/"
-        a.call_playlist(key, id).to_csv(folder_to_export_path+ "testing.csv", encoding = "utf-8-sig")
+        a.playlist_sql("songstats.db", key, id)
+
 
 main()
