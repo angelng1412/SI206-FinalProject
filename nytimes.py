86a0bdad043d67d621054b060d3ef81554c46176
Joan Suh <93625433+joansuh12@users.noreply.github.com>
2022-04-26 17:02:08 -0400

    Update nytimes.py

--- nytimes.py
+++ nytimes.py
@@ -24,8 +24,8 @@ def fetchData(db_name, conn, cur):
     # database connection
     conn, cur = database(db_name)
     # pull artist names from db
-    cur.execute("""SELECT artist FROM artistinfo""")
-    artists = cur.fetchall()
+    res = cur.execute("""SELECT artist FROM artistinfo""")
+    artists = res.fetchall()
     artists = [x[0] for x in artists]
     # print(artists)
     news_lst = {}
@@ -46,11 +46,12 @@ def fetchData(db_name, conn, cur):
         news_lst[artist] = artist_result
     return news_lst
 
-# fetchData("songstats.db")
+# curr, con = database("songstats.db")
+# fetchData("songstats.db", curr, con)
 
 def setUpNewsTable(news_lst, cur, conn):
     cur.execute("DROP TABLE IF EXISTS nynews")
-    cur.execute("""CREATE TABLE IF NOT EXISTS nynews (name TEXT UNIQUE PRIMARY KEY, news_total INTEGER)""")
+    cur.execute("""CREATE TABLE IF NOT EXISTS nynews (name TEXT UNIQUE PRIMARY KEY, article_total INTEGER)""")
     conn.commit()
     cur.execute("""SELECT artist FROM artistinfo""")
     artists = cur.fetchall()
@@ -64,7 +65,7 @@ def setUpNewsTable(news_lst, cur, conn):
 
 def main():
     cur, conn = database('songstats.db')
-    news_lst = fetchData("songstats.db", conn, cur)
+    news_lst = fetchData("songstats.db", cur, conn)
     setUpNewsTable(news_lst, cur, conn)
 
 main()
